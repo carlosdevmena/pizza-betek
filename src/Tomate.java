a12cfb8f73266273e14e5cf412a24a5d857b5b97
Carlos Dev <111801804+carlosdevmena@users.noreply.github.com>
2024-07-31 16:18:01 -0500

    Update Tomate.java

--- src/Tomate.java
+++ src/Tomate.java
@@ -2,11 +2,11 @@ package src;
 
 public class Tomate {
   public static void main(String[] args) {
-        System.out.println("Hola, soy un tomate. El mejor tomate del mundo xd!");
-        System.out.println("Pronto sere un Object() y tendre atributos, contructor, instancias :))");
+        // System.out.println("Hola, soy un tomate. El mejor tomate del mundo xd!");
+        // System.out.println("Pronto sere un Object() y tendre atributos, contructor, instancias :))");
     }
 
     public void agregar() {
-        System.out.println("Mozzarella agregada a la pizza.");
+        System.out.println("Tomate agregada a la pizza.");
     }
 }
